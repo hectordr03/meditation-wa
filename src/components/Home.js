cf8292003cf7f8212e346bffd73a768db2faf674
Hectordr03 <Hectordr03@gmail.com>
2021-03-31 11:08:38 -0400

    add splash page svgs

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Home.scss';
 
 const Home = () => {
-    return (
-        <div>
-            <h2>I am the Home component</h2>
-        </div>
-    );
-}
+	return (
+		<div>
+			<h2 className='compTitle'>I am the Home component</h2>
+			<div className='splash'>
+            </div>
+		</div>
+	);
+};
 
 export default Home;
