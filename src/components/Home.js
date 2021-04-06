79d957e60e921ca3ae3d60c0d38a90256ff09401
Hectordr03 <Hectordr03@gmail.com>
2021-04-06 08:01:38 -0400

    created timer comp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Home.scss';
+import Timer from './Timer';
 
 const Home = () => {
 	return (
 		<div>
 			<p className='compTitle'>I am the Home component, plus some styyyle</p>
 			<div className='splash'>
-				
+				<Timer />
 			</div>
 		</div>
 	);
