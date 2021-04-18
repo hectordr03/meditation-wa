0dd61ac92ce80d621e9ea4ff619538830ec52b44
Hectordr03 <Hectordr03@gmail.com>
2021-04-18 10:45:43 -0400

    background-color not working on timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import '../components/Timer.scss';
+import CountdownTimer from '@sakit-sa/countdown-timer';
+
+// Timer component sourced from https://www.skypack.dev/view/@sakit-sa/countdown-timer
+
+// How to use sakit timer: 
+
+// import CountdownTimer from '@sakit-sa/countdown-timer';
+
+//  <CountdownTimer 
+//    time={90}
+//    format="[hh]:[mm]:[ss]"
+//    onComplete={() => console.log("The timer completed")} 
+//  />
 
 class Timer extends React.Component {
     render () {
         return(
             <div className='wrapper'>
                 <p className='title'>Timer component</p>
+                <CountdownTimer className='timer'/>
             </div>
         );
     }
