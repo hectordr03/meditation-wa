c1bb36641307570707fd15fb1b964f9e02089e65
Hectordr03 <Hectordr03@gmail.com>
2021-04-18 11:36:56 -0400

    switching to using state to conditionally render timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -9,20 +9,63 @@ import CountdownTimer from '@sakit-sa/countdown-timer';
 // import CountdownTimer from '@sakit-sa/countdown-timer';
 
 //  <CountdownTimer 
-//    time={90}
+//    time={90} // in seconds
 //    format="[hh]:[mm]:[ss]"
 //    onComplete={() => console.log("The timer completed")} 
 //  />
 
 class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showTimer = false,
+        };
+    }
+    
+    // timer1m30s = () => {
+    //     return (
+    //     <CountdownTimer 
+    //     className='timer'
+    //     time={90}
+    //     format='MM:SS'
+    //     onComplete={() => alert("Good job! You finished your meditation. (Meditative quote here, I like what headspace says about the always present blue sky)")}/>)
+    // };
+
+    // timer3m = () => {
+    //     <CountdownTimer 
+    //     className='timer'
+    //     time={180}
+    //     format='MM:SS'
+    //     onComplete={() => alert("Good job! You finished your meditation. (Meditative quote here, I like what headspace says about the always present blue sky)")}/>
+    // };
+    // timer5m = () => {
+    //     <CountdownTimer 
+    //     className='timer'
+    //     time={300}
+    //     format='MM:SS'
+    //     onComplete={() => alert("Good job! You finished your meditation. (Meditative quote here, I like what headspace says about the always present blue sky)")}/>
+    // };
+
     render () {
         return(
             <div className='wrapper'>
                 <p className='title'>Timer component</p>
-                <CountdownTimer className='timer'/>
+                {/* <CountdownTimer 
+                className='timer'
+                format='MM:SS'
+                onComplete={() => alert("Good job! You finished your meditation. (Meditative quote here, I like what headspace says about the always present blue sky)")}/> */}
+
+                <button onClick={this.timer1m30s}>1m 30s</button>
             </div>
         );
     }
 }
 
 export default Timer;
+
+
+// I need a component to hold the coundown timer at 00:00 while the user chooses how long the timer should be
+
+// Create functions that render <Countdown Timer /> with corresponding "time" props
+
+// Attempt pause/play feature
